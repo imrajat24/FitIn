500db19cdcb67dd775fc040341292a5f0ed4cd16
imrajat24 <naman.sahni@gamingcentral.in>
2022-05-08 00:43:07 +0530

    Created the basic structure of the project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Home from "./Pages/Home";
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>
+      <div>this is from home</div>
+    </>
+  );
+}
+
+export default Home;
